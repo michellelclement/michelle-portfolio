0113affc0e6b51a0ad3c591cafe9ca0e79997282
Michelle Clement <65297933+michellelclement@users.noreply.github.com>
2023-11-16 10:28:42 +0000

    Start animation testing

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,5 +1,6 @@
 // import { welcome } from './modules/welcome';
 import burgerMenu from './modules/burger-menu';
 import images from './modules/images';
+import textAnimations from './modules/text-animations';
 
 // welcome();
---
+++ src/assets/scripts/modules/text-animations.js
@@ -0,0 +1,5 @@
+import { gsap } from "gsap";
+
+// gsap.from(".hero__header-1, .hero__header-2, .hero__sub-header", {duration: 2, x: 300});
+
+gsap.from(".hero__text", {duration: 2, opacity: 0, stagger: 0.5});
