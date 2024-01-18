be5c551d501476d300149a3842a9c0e0ad8774bc
Michelle Clement <65297933+michellelclement@users.noreply.github.com>
2024-01-18 11:41:00 +0000

    Animate content in fixed. Shape animation to correct

--- src/assets/scripts/modules/page.js
+++ src/assets/scripts/modules/page.js
@@ -38,24 +38,55 @@ function heroAnimation() {
   tl.from(".js-hero-animate-this", { duration: 1, y: 30, opacity: 0, stagger: 0.4, delay: 0.2 });
 }
 
+function animateTextInSections() {
+  const items = Array.from(document.querySelectorAll('.js-animate-in, .js-animate-in-children > *'));
+  if (items.length) {
+    ScrollTrigger.refresh();
+    ScrollTrigger.batch(items, {
+      start: 'top 80%',
+      end: 'bottom 20%',
+      onEnter: (els) => {
+        gsap.to(els, {
+          y: 0,
+          opacity: 1,
+          duration: 0.3,
+          stagger: 0.1,
+          ease: 'back.out(1.1)'
+        });
+      },
+      once: true,
+    });
+  }
+}
+
+animateTextInSections();
+
 barba.init({
   sync: true,
   transitions: [
     {
       async leave(data) {
         const done = this.async();
-
         pageTransition();
         await delay(1000);
         done();
       },
 
       async enter(data) {
+        // Ensure that the elements are in the DOM before initiating the animation
+        await delay(500); // Adjust the delay as needed
         heroAnimation();
       },
 
       async once(data) {
-          heroAnimation();
+        // Ensure that the elements are in the DOM before initiating the animation
+        await delay(500); // Adjust the delay as needed
+        heroAnimation();
+      },
+
+      async after(data) {
+        // Reattach event listeners or perform other tasks after the new page is added to the DOM
+        animateTextInSections();
       },
     }
   ]
@@ -66,23 +97,3 @@ let smoother = ScrollSmoother.create({
   smooth: 1.5,
   smoothTouch: 0.1
 });
-
-// Animate in text
-// const items = Array.from(document.querySelectorAll('.js-animate-in, .js-animate-in-children > *'));
-// if (items.length) {
-//   ScrollTrigger.refresh();
-//   ScrollTrigger.batch(items, {
-//     start: 'top 80%', // Adjust this value to change when the animation starts
-//     end: 'bottom 20%',
-//     onEnter: (els) => {
-//       gsap.to(els, {
-//         y: 0,
-//         opacity: 1,
-//         duration: 0.3,
-//         stagger: 0.1,
-//         ease: 'back.out(1.1)'
-//       });
-//     },
-//     once: true,
-//   });
-// }
